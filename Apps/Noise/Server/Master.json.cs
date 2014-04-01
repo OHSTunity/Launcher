20f84d58b59ff6aa3bdddaea04c4758944110f74
Tomek Wytrebowicz <tomalecpub@gmail.com>
2014-04-01 23:55:38 +0200

    Try to make Noise respond to other requests as well (issue #14)

--- Apps/Noise/Server/Master.json.cs
+++ Apps/Noise/Server/Master.json.cs
@@ -2,15 +2,21 @@ using System;
 using Starcounter;                           // Most stuff relating to the database, JSON and communication is in this namespace
 
 [Master_json]                                       // This attribute tells Starcounter that the class corresponds to an object in the JSON-by-example file.
-partial class Master : Page {
+partial class Master : Page
+{
 
     /// <summary>
     /// Every application in Starcounter works like a console application. They have an .EXE ending. They have a Main() function and
     /// they can do console output. However, they are run inside the scope of a database rather than connecting to it.
     /// </summary>
-    static void Main() {
+    static void Main()
+    {
 
-        Handle.GET("/super-crm/partials/companies/{?}", (String url) => 
+        // Handle.GET("/super-crm/partials/companies/{?}", (String url) =>  // <-- this works
+        // Handle.GET("/super-crm/partials/companies/{?}", (String url) =>  // <-- this doesn't
+        // Handle.GET("/super-crm/partials/{?}/{?}", (String name, String url) =>  // <-- nope
+        // Handle.GET("/super-crm/{?}", (String name, String url) =>  // <-- nope
+        Handle.GET("/{?}", (String url) => // <-- nope
         {
             var page = new Page()
             {
