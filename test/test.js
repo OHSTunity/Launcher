0e691789e2845fe19e11004e0e18d0c6ca21a9dc
Pär Eriksson <parherman@gmail.com>
2015-03-09 16:28:22 +0100

    add simple mocha tests

---
+++ test/test.js
@@ -0,0 +1,51 @@
+var assert = require('assert'),
+    request = require('supertest'),
+    cheerio = require('cheerio'),
+    app = 'http://localhost:8080'
+
+describe('GET /', function(){
+    it('Shuold respond with html when Accept="text/html"', function(done){
+        request(app)
+        .get('/')
+        .set('Accept', 'text/html')
+        .expect('Content-Type', /html/)
+        .expect(200, done);
+    })
+    it('<title> should be "Launcher"', function(done){
+    request(app)
+        .get('/')
+        .set('Accept', 'text/html')
+        .expect('Content-Type', /html/)
+        .expect(200)
+        .expect(function(res) {
+            var $ = cheerio.load(res.text);
+            assert.equal('Launcher', $('title').html());
+        })
+        .end(function(err, res){
+            if (err) return done(err);
+            done()
+        });
+    })
+    it('Should respond with json when Accept="application/json"', function(done){
+        request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    })
+    it('Json respond should contain Launcher.results"', function(done){
+        request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(function(res) {
+            var json = JSON.parse(res.text)
+            assert(json.Launcher.results);
+        })
+        .end(function(err, res){
+            if (err) return done(err);
+            done()
+        });
+    })
+})
