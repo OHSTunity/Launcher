788ddcfcd843c2dd5212c1a2cacddc4148ca2a02
Pär Eriksson <parherman@gmail.com>
2015-03-25 09:33:35 +0100

    Update AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PMail")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Launcher")]
+[assembly: AssemblyDescription("Launcher Application (tested with Starcounter 2.0.3094.3)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PMail")]
+[assembly: AssemblyCompany("Starcounter")]
+[assembly: AssemblyProduct("Launcher")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0.0-pre5")]
