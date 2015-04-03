c6454dfd8c6b26765eb5bc17a08a7e8a5dde1f55
miyconst <const.miyang@gmail.com>
2015-04-03 23:09:35 +0300

    Add test to check application name case sensitivity
    Add "Application name should be case insensitive" test.
    https://github.com/Polyjuice/Launcher/issues/110

--- test/test.js
+++ test/test.js
@@ -40,12 +40,61 @@ describe('Launcher App', function () {
         .expect('Content-Type', /json/)
         .expect(200)
         .expect(function (res) {
-            var json = JSON.parse(res.text)
+            var json = JSON.parse(res.text);
             assert(json.results);
         })
         .end(function (err, res) {
             if (err) return done(err);
             done();
+        });
+    });
+
+    //Requires SignIn app running
+    it("Application name should be case insensitive", function (done) {
+        var test = function (url, referer, callback) {
+            request(app)
+            .get(url)
+            .set('Accept', 'application/json-patch+json')
+            .set("X-Referer", referer)
+            .expect('Content-Type', "application/json-patch+json")
+            .expect(200)
+            .expect(function (res) {
+                var json = JSON.parse(res.text.replace(/,,/gi, ","));
+                var patch = json[json.length - 1];
+                var reg = new RegExp("^/workspaces/0", "gi");
+
+                assert(reg.test(patch.path), "Application name should be case insensitive!");
+            })
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+
+                callback();
+            });
+        };
+
+        request(app)
+        .get('/signin/signinuser')
+        .set('Accept', 'application/json-patch+json')
+        .expect('Content-Type', 'application/json-patch+json')
+        .expect(200)
+        .end(function (err, res) {
+            var cookies = res.header["set-cookie"];
+            var referer;
+
+            for (var i = 0; i < cookies.length; i++) {
+                if (/^Location/gi.test(cookies[i])) {
+                    referer = cookies[i].replace(/^Location=/gi, "").replace(new RegExp("; path=/$"), "").replace(/%2F/gi, "/");
+                    break;
+                }
+            }
+
+            test("/signin/signin/username/password", referer, function () {
+                test("/signIn/signin/username/password", referer, function () {
+                    done();
+                });
+            });
         });
     });
 });
