0818d7c0fefb07f499e839cdeec2691de5467091
David Rosendahl <david.rosendahl@gmail.com>
2017-03-21 13:36:11 +0100

    not sure

--- src/Launcher/Program.cs
+++ src/Launcher/Program.cs
@@ -13,13 +13,7 @@ namespace Launcher {
             LauncherHelper.Init();
             var port = Starcounter.Internal.StarcounterEnvironment.Default.UserHttpPort;
             ThreadPool.QueueUserWorkItem(o => {
-                
                 Http.GET(8181, String.Format("http://localhost/sc/alias/{0}/;/launcher/container_dashboard", port));
-                Http.GET(8181, String.Format("http://localhost/sc/alias/{0}/dashboard;/launcher/container_dashboard", port));
-                Http.GET(8181, String.Format("http://localhost/sc/alias/{0}/settings;/launcher/settings", port));
-                Http.GET(8181, String.Format("http://localhost/sc/alias/{0}/configs;/launcher/configs", port));
-                Http.GET(8181, String.Format("http://localhost/sc/alias/{0}/mobile;/launcher/mobile", port));
-                Http.GET(8181, String.Format("http://localhost/sc/alias/{0}/mobile/dashboard;/launcher/mobile/dashboard", port));
             });
         }
     }
